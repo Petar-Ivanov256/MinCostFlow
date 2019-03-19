dcbb32b16d094ee55e4316f74fc20fe7801a7097
Petar Ivanov <petar.ivanov256@gmail.com>
2019-03-19 08:18:42 +0200

    Improving the UI

--- opticost/src/main/resources/static/js/script.js
+++ opticost/src/main/resources/static/js/script.js
@@ -1,14 +1,25 @@
-$("#addCityBtn").on('click', addCity);
-$("#saveCities").on('click', saveCities);
-var addedCities = [];
+init();
+let addedCities = [];
+let addedRoads = [];
+let graph = [];
+let nodeSize = 3;
+let nodeColor = '#008cc2';
+
+function init() {
+    $("#sigma-container").css("height", $(document).height() * 0.9);
+    $("#addCityBtn").on('click', addCity);
+    $("#saveCities").on('click', saveCities);
+    $("#addRoad").on('click', addRoad);
+    $("#saveRoad").on('click', addRoad);
+}
 
 function addCity() {
-    var cityName = $("#inputCityName").val();
-    var xCoord = $("#inputX").val();
-    var yCoord = $("#inputY").val();
-    var showTable = $("#showCities");
+    let cityName = $("#inputCityName").val();
+    let xCoord = $("#inputX").val();
+    let yCoord = $("#inputY").val();
+    let showTable = $("#showCities");
 
-    var previousHtml = showTable.html();
+    let previousHtml = showTable.html();
     showTable.html(
         previousHtml +
         "<tr>" +
@@ -33,8 +44,44 @@ function addCity() {
     );
 }
 
+function addRoad() {
+    let fromCity = $("#fromCity").val();
+    let toCity = $("#toCity").val();
+    let cap = $("#inputCap").val();
+    let price = $("#inputPrice").val();
+    let showTable = $("#showRoads");
+
+    let previousHtml = showTable.html();
+    showTable.html(
+        previousHtml +
+        "<tr>" +
+        "<td class='col-md-3'>" +
+        fromCity +
+        "</td>" +
+        "<td class='col-md-3'>" +
+        toCity +
+        "</td>" +
+        "<td class='col-md-3'>" +
+        cap +
+        "</td>" +
+        "<td class='col-md-3'>" +
+        price +
+        "</td>" +
+        "</tr>"
+    );
+
+    addedRoads.push(
+        {
+            'fromCity': fromCity,
+            'toCity': toCity,
+            'capacity': cap,
+            'price': price
+        }
+    );
+}
+
 function saveCities() {
-    console.log(addedCities)
+    console.log(addedCities);
     $.ajax({
         type: "POST",
         url: "/save-cities",
@@ -42,9 +89,61 @@ function saveCities() {
         contentType: "application/json; charset=utf-8",
         success: function (data) {
             console.log("Success", data);
+            drawGraph(data);
         },
         error: function (data) {
             console.log("Error", data);
         }
     });
+}
+
+function saveRoads() {
+    console.log(addedRoads);
+    // $.ajax({
+    //     type: "POST",
+    //     url: "/save-cities",
+    //     data: JSON.stringify(addedCities),
+    //     contentType: "application/json; charset=utf-8",
+    //     success: function (data) {
+    //         console.log("Success", data);
+    //         drawGraph(data);
+    //     },
+    //     error: function (data) {
+    //         console.log("Error", data);
+    //     }
+    // });
+}
+
+function drawGraph(cities) {
+    // Initialise sigma:
+    let s = new sigma(
+        {
+            renderer: {
+                container: document.getElementById('sigma-container'),
+                type: 'svg'
+            },
+            settings: {
+                minEdgeSize: 0.1,
+                maxEdgeSize: 2,
+                minNodeSize: 1,
+                maxNodeSize: 8,
+            }
+        }
+    );
+
+    let nodes = cities.map(function (x) {
+        return {id: x.id, label: x.cityName, x: x.xCoord, y: x.yCoord, size: nodeSize, color: nodeColor}
+    });
+
+// Create a graph object
+    graph = {
+        nodes: nodes
+    };
+
+// Load the graph in sigma
+    s.graph.read(graph);
+// Ask sigma to draw it
+    s.refresh();
+
+    console.log("Drawing graph")
 }
