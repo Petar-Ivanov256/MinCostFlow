b88d2b067b91f91c1e1bb1fa2fe166328a443819
Petar Ivanov <petar.ivanov256@gmail.com>
2021-03-25 14:08:56 +0200

    Random graph generation
    A class for random graph generation is added. The algorithm is based on
    the first algorithm find in the article Fast Random Graph Generation,
    National University of Singapore with constraints for self vertex loops
    and parallel edges.
    
    Second class for time bench marking of the Mincost Flow algorithm with
    the random generated graph is added.

---
+++ opticost/src/main/java/GraphLimitsTester.java
@@ -0,0 +1,48 @@
+import com.project.opticost.algorithm.Graph;
+import com.project.opticost.algorithm.Vertex;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class GraphLimitsTester {
+    public static void main(String[] args) throws Exception {
+        RandomGraphGenerator rgg = new RandomGraphGenerator();
+        int n = 60;
+        double prob = 0.05d;
+
+        Graph g = rgg.generateGraph(n, prob);
+        System.out.println("Finish generating the graph");
+        System.out.println("Number of vertices: " + g.getListOfVertices().size());
+        System.out.println("Number of edges: " + g.getListOfEdges().size());
+        long startTime = System.nanoTime();
+        g.minCostFlowCycleCancel(new Vertex("1"), new Vertex(String.valueOf(n)), 10);
+        long stopTime = System.nanoTime();
+        long timeElapsed = stopTime - startTime;
+        System.out.println("Execution time in milliseconds : " + timeElapsed / 1000000);
+        System.out.println("Execution time in seconds : " + ((timeElapsed / 1000000) / 1000) % 60);
+        System.out.println("Execution time in minutes : " + ((timeElapsed / 1000000) / (1000 * 60)) % 60);
+        g.getResult();
+        System.out.println("Mincost flow is: " + g.getMinCostFlow());
+        System.out.println("Debug");
+
+        createCsv(rgg.getFileEdges());
+    }
+
+    public static void createCsv(List<String> data) {
+        StringBuilder sb = new StringBuilder();
+        for (String line : data) {
+            sb.append(line);
+            sb.append('\n');
+        }
+
+        try (PrintWriter writer = new PrintWriter(new File("plan.csv"))) {
+            writer.write(sb.toString());
+
+            System.out.println("done!");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ opticost/src/main/java/RandomGraphGenerator.java
@@ -0,0 +1,57 @@
+import com.project.opticost.algorithm.Edge;
+import com.project.opticost.algorithm.Graph;
+import com.project.opticost.algorithm.Vertex;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class RandomGraphGenerator {
+    private HashMap<Integer, Vertex> vertices = new HashMap<>();
+    private HashSet<Edge> edges = new HashSet<>();
+    private List<String> fileEdges = new ArrayList<>();
+    private String NAME_PREFIX = "bex";
+
+    public Graph generateGraph(int numberOfVertices, double inclusionProbability) {
+        Graph g = new Graph();
+        for (int i = 1; i <= numberOfVertices; i++) {
+            Vertex from = getVertex(i);
+            for (int j = 1; j <= numberOfVertices; j++) {
+                Vertex to = getVertex(j);
+                Integer capacity = new Random().nextInt(50);
+                BigDecimal price = BigDecimal.valueOf(new Random().nextInt(50));
+                Edge edge = new Edge(from, to, capacity, price);
+
+                double probability = new Random().nextDouble();
+
+                if (probability < inclusionProbability && i != j && !isParallelEdge(edge)) {
+                    g.addEdge(edge);
+                    fileEdges.add(NAME_PREFIX + from.getName() + "," + NAME_PREFIX + to.getName() + "," + capacity + "," + price);
+                }
+            }
+
+        }
+        return g;
+    }
+
+    public List<String> getFileEdges() {
+        return fileEdges;
+    }
+
+    private Vertex getVertex(int vertex) {
+        if (vertices.containsKey(vertex)) {
+            return vertices.get(vertex);
+        } else {
+            vertices.put(vertex, new Vertex(String.valueOf(vertex)));
+            return vertices.get(vertex);
+        }
+    }
+
+    private Boolean isParallelEdge(Edge edge) {
+        if (edges.contains(edge)) {
+            return true;
+        } else {
+            edges.add(edge);
+            return false;
+        }
+    }
+}
